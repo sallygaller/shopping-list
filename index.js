af264930b5bc6755aa86a053e589b62ac6d89fdd
sallygaller <65969767+sallygaller@users.noreply.github.com>
2020-07-18 14:51:10 -0700

    Upload 3

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ $(function() {
     //store new list value from what was input
     const newListInput = $(this).find(
     'input[name="shopping-list-entry"]').val();
-    const pressedInput = $(newListInput).attr('aria-pressed') === 'true';
     //create new list item variable, same classes as other list items
     const newListItem = '<li> <span class="shopping-item">' + newListInput + '</span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>';
     //append the variable to the shopping list
     $('.shopping-list').append(newListItem);
+    //pass input an empty value to clear input field
+    $(this).find('input[name="shopping-list-entry"]').val("");
     });  
 
     //listen for click on 'check' button
